53e2d4f1d76bfde2e928b8378ec95c1e7f708f94
chisom <chisom.ofoedu@flutterwavego.com>
2022-09-28 15:09:34 +0100

    Refactor based on feedback review

--- controllers/postAnswer.js
+++ controllers/postAnswer.js
@@ -1,15 +1,14 @@
 const models = require("../models/index");
-const createAnswer = async(req, res) => {
-    try{
-        const { answer_text } = req.body;
+const createAnswer = async (req, res) => {
+    try {
+        const { answer_text, question_id } = req.body;
         const user_id = req.user_id;
-        const { question_id }= req.body;
         const answer = await models.answers.create({
             "answer_text": answer_text,
             "user_id": user_id,
             "question_id": question_id
-    });
-    return res.json(answer);
+        });
+        return res.json(answer);
     } catch (err) {
         return res.status(500).json({ messages: err.message })
     }
