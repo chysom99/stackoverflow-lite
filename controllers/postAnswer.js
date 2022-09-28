e5c1ab2f693909bf030aa24daf7f50df8a60381f
chisom <chisom.ofoedu@flutterwavego.com>
2022-09-28 11:19:58 +0100

    Api endpoint for posting answers

--- controllers/postAnswer.js
+++ controllers/postAnswer.js
@@ -3,9 +3,9 @@ const createAnswer = async(req, res) => {
     try{
         const { answer_text } = req.body;
         const user_id = req.user_id;
-        const question_id = req.question_id;
+        const { question_id }= req.body;
         const answer = await models.answers.create({
-            "answer.text": answer_text,
+            "answer_text": answer_text,
             "user_id": user_id,
             "question_id": question_id
     });
