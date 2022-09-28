831a743828d6de0ea608859057f1be183c3428a2
chisom <chisom.ofoedu@flutterwavego.com>
2022-09-28 11:20:55 +0100

    added a postAnswer route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const { signup, login } = createUserController
 const userAuth = require('../middleware/auth.js')
 const auth_token = require('../middleware/auth_token')
 const postQuestion = require('../controllers/postQuestion')
+const postAnswer = require('../controllers/postAnswer')
+
 
 router.get('/', (req, res) => {
     res.send("I got you covered, hit me any time!! ");
@@ -13,6 +15,7 @@ router.get('/', (req, res) => {
 router.post('/signup', userAuth.validateUser, signup)
 router.post('/login', login )
 router.post('/question', auth_token, postQuestion.createQuestion)
+router.post('/answer', auth_token, postAnswer.createAnswer)
 //router.post('/users', createUserController);
 
 
