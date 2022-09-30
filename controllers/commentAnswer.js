ee1460e28013661516927b3510b4195c6c749cd3
chisom <chisom.ofoedu@flutterwavego.com>
2022-09-30 11:22:10 +0100

    An api endpoint for commenting on an Answer

---
+++ controllers/commentAnswer.js
@@ -0,0 +1,33 @@
+const models = require('../models/index');
+const commentAnswer = async (req, res) => {
+    try {
+        const commentId = parseInt(req.params.id);
+        const { comment_text, answer_id } = req.body;
+        const user_id = req.user_id;
+
+        //check if the answer exists
+        const answer = await models.answers.findOne({
+            where: {
+                id: answer_id,
+            },
+        });
+
+        if (answer) {
+            const comment = await models.comments.create({
+                comment_text: comment_text,
+                id: commentId,
+                user_id: user_id,
+                answer_id: answer_id,
+            });
+            return res.status(200).json(comment);
+        } else {
+            return res.status(404).json('Answer does not exist');
+        }
+    } catch (err) {
+        return res.status(500).json({ messages: err.message });
+    }
+};
+
+module.exports = {
+    commentAnswer: commentAnswer,
+};
