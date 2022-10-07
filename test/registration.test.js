4936db1df0a696d96c2f45397fed6109221305c8
chisom <chisom.ofoedu@flutterwavego.com>
2022-10-07 06:50:01 +0100

    test file for user registration

---
+++ test/registration.test.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const supertest = require('supertest');
+const app = require('../index');
+
+describe('User Registration Test', function () {
+    describe('Positive Tests', function () {
+        it('should register user successfully', async function () {
+            const response = await supertest(app)
+                .post('/api/v1/signup')
+                .send({
+                    username: 'chisom',
+                    firstname: 'lizzy',
+                    lastname: 'john',
+                    email: Date.now() + 'ofoeduc7@gmail.com',
+                    password: 'ofoedu1',
+                })
+                .set('Accept', 'application/json')
+                .expect(201);
+
+            const resp_data = response.body;
+            console.log(resp_data);
+            expect(resp_data).to.be.an('object');
+            expect(resp_data).to.have.property('user');
+            expect(resp_data.user.id).to.be.an('number');
+        });
+    });
+
+    describe('Negative Tests', function () {
+        it('should not register user successfully when an email is reuse', async function () {
+            const response = await supertest(app)
+                .post('/api/v1/signup')
+                .send({
+                    username: 'chisom',
+                    firstname: 'lizzy',
+                    lastname: 'john',
+                    email: 'ofoeduc7@gmail.com',
+                    password: 'ofoedu1',
+                })
+                .set('Accept', 'application/json')
+                .expect(409);
+
+            const resp_data = response.body;
+            expect(resp_data).to.be.an('object');
+            expect(resp_data.message).to.be.an('string');
+            expect(resp_data.message).to.contain('Email already taken');
+        });
+    });
+});
