7215081ef85c563971793df116a57155cf182d92
chisom <chisom.ofoedu@flutterwavego.com>
2022-10-07 06:44:30 +0100

    minor change

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const db = require('./models/index');
 const routes = require('./routes/index');
-const sequelize = require('sequelize')
-const dotenv = require('dotenv').config()
-const cookieParser = require('cookie-parser')
+const sequelize = require('sequelize');
+const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api/v1/',routes);
+app.use('/api/v1/', routes);
 
 const PORT = process.env.PORT || 4040;
 
-db.sequelize.sync().then(() => {
-    console.log('Database Synced successfully')
-}).catch((err) => {
-    console.log('Database synchronization failed, ' + err)
-})
-
+db.sequelize
+    .sync()
+    .then(() => {
+        console.log('Database Synced successfully');
+    })
+    .catch((err) => {
+        console.log('Database synchronization failed, ' + err);
+    });
 
 app.listen(PORT, () => {
-    console.log("Server started on port #" + PORT)
-})
+    console.log('Server started on port #' + PORT);
+});
 
+module.exports = app;
