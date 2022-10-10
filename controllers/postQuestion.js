32b7f32e340a71864e97b982db914ae6b3991ee8
chisom <chisom.ofoedu@flutterwavego.com>
2022-10-10 07:27:43 +0100

    Refactoring based on review feedback

--- controllers/postQuestion.js
+++ controllers/postQuestion.js
@@ -1,18 +1,22 @@
-const models = require("../models/index");
-const createQuestion = async(req, res) => {
+const models = require('../models/index');
+const createQuestion = async (req, res) => {
     try {
         const { question_text } = req.body;
+        if (!question_text || !question_text.trim())
+            return res
+                .status(400)
+                .json({ messages: 'Question text is required' });
         const user_id = req.user_id;
         const question = await models.questions.create({
-            "question_text": question_text,
-            "user_id": user_id
+            question_text: question_text,
+            user_id: user_id,
         });
         return res.json(question);
     } catch (err) {
-        return res.status(500).json({ messages: err.message })
+        return res.status(500).json({ messages: err.message });
     }
-}
+};
 
 module.exports = {
-    "createQuestion": createQuestion
-}
+    createQuestion: createQuestion,
+};
