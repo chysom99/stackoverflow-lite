98284242d82eb8140f5bc591f84aa1123c0d7b24
chisom <chisom.ofoedu@flutterwavego.com>
2022-10-12 09:03:32 +0100

    added postive and negative test for voting on an answer

--- test/Answer.test.js
+++ test/Answer.test.js
@@ -32,14 +32,12 @@ describe('Answer Test', function () {
     //positive test for viewing answer
     it('should successfully view an answer', async function () {
         const response = await supertest(app)
-            .get('/api/v1/answer/:question_id')
+            .get('/api/v1/answer/19')
             .set({
                 Authorization: sample_data.auth_token,
                 Accept: 'application/json',
             })
-            .send({
-                question_id: '19',
-            })
+
             .expect(200);
         const resp_data = response.body;
         expect(resp_data).to.be.an('object');
@@ -67,6 +65,25 @@ describe('Answer Test', function () {
         expect(resp_data.user_id).to.be.an('number');
     });
 
+    //positive test for voting on an answer
+    it('should successfully vote on an answer', async function () {
+        const response = await supertest(app)
+            .put('/api/v1/vote')
+            .set({
+                Authorization: sample_data.auth_token,
+                Accept: 'application/json',
+            })
+            .send({
+                vote_type: 'down',
+                answer_id: '9',
+            })
+            .expect(200);
+        const resp_data = response.body;
+        expect(resp_data).to.be.an('object');
+        expect(resp_data).to.have.property('vote_type');
+        expect(resp_data.vote_type).to.be.an('string');
+    });
+
     //negative test for post answer
     it('should not successfully post an answer', async function () {
         const response = await supertest(app)
@@ -87,8 +104,7 @@ describe('Answer Test', function () {
         expect(resp_data.messages).to.contain('Answer text is required');
     });
 
-    //negative test for commenting on an answer
-    it('should not successfully comment an answer', async function () {
+    it('should not successfully comment on answer when answer id is not passed', async function () {
         const response = await supertest(app)
             .post('/api/v1/comment')
             .set({
@@ -104,6 +120,45 @@ describe('Answer Test', function () {
         const resp_data = response.body;
         expect(resp_data).to.be.an('object');
         expect(resp_data.messages).to.be.an('string');
+        expect(resp_data.messages).to.contain('Answer id is required');
+    });
+
+    //negative test for commenting on an answer
+    it('should not successfully comment on answer when answer does not exist', async function () {
+        const response = await supertest(app)
+            .post('/api/v1/comment')
+            .set({
+                Authorization: sample_data.auth_token,
+                Accept: 'application/json',
+            })
+            .send({
+                comment_text: 'grace',
+                user_id: '24',
+                answer_id: '-1',
+            })
+            .expect(404);
+        const resp_data = response.body;
+        expect(resp_data).to.be.an('object');
+        expect(resp_data.messages).to.be.an('string');
         expect(resp_data.messages).to.contain('Answer does not exist');
+    });
+
+    //negative test for voting on an answer
+    it('should not vote on an answer when answer id is not found', async function () {
+        const response = await supertest(app)
+            .put('/api/v1/vote')
+            .set({
+                Authorization: sample_data.auth_token,
+                Accept: 'application/json',
+            })
+            .send({
+                vote_type: 'down',
+                answer_id: '-9',
+            })
+            .expect(404);
+        const resp_data = response.body;
+        expect(resp_data).to.be.an('object');
+        expect(resp_data.message).to.be.an('string');
+        expect(resp_data.message).to.contain('Answer was not found');
     });
 });
